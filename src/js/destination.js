633fd90f36e4ecdd629997325010d66c5288c08c
gamerviker <visakhsanjeev32@gmail.com>
2025-12-19 10:05:28 +0530

    day3 done

--- src/js/destination.js
+++ src/js/destination.js
@@ -4,8 +4,48 @@ const city = params.get("city");
 
 const cityName = document.getElementById("cityName");
 
-if(city) {
-    cityName.textContent = city;
+let GEODB_API_KEY = "729cfb446amsh7c28abd4fb84ddfp1b52f3jsnde05037bfc40";
+let GEODB_HOST = "wft-geo-db.p.rapidapi.com";
+
+if (city) {
+  cityName.textContent = city;
+  async function fetchCityDetails(cityName) {
+    try {
+      let response = await fetch(
+        `https://${GEODB_HOST}/v1/geo/cities?namePrefix=${cityName}&limit=1`,
+        {
+          method: "GET",
+          headers: {
+            "X-Rapidapi-Key": GEODB_API_KEY,
+            "X-Rapidapi-Host": GEODB_HOST,
+          },
+        }
+      );
+
+    //   console.log(response);
+
+        if (!response.ok) {
+            console.log("API not fetched....");
+        }
+
+        let data = await response.json();
+        console.log(data);
+        if(!data.data || data.data.length === 0) {
+            console.log("City not found");
+        }
+        let cityData = data.data[0];
+        console.log(cityData);
+        
+            
+            
+
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  fetchCityDetails(city);
 } else {
-    cityName.textContent = "Unknown City";
+  cityName.textContent = "Unknown City";
 }
